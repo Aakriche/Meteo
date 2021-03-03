33d1534bffdbf2c1120252cc6eff527024de1202
Amaury AKRICHE <32291662+Margulda@users.noreply.github.com>
2021-03-03 10:40:53 +0100

    Add files via upload

--- meteo.js
+++ meteo.js
@@ -3,37 +3,40 @@ function askWeather() {
 
   
     var request = new XMLHttpRequest();
-     request.onreadystatechange = function() {
+    request.onreadystatechange = function() {
        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
            var response = JSON.parse(this.responseText);
          console.log(response.current_condition.tmp);
          
-    document.getElementById('weather-result').innerHTML = response.current_condition.tmp;
+          document.getElementById('weather-result').innerHTML = response.current_condition.tmp;
     
-    for ( var i=0; i<=23; i++) {
-        newTR = document.createElement("tr");
-        newTdh = document.createElement("td");
-        newTd = document.createElement("td");
-        newTdh.innerHTML =i+"H00"
-        newTd.setAttribute("id", "tmp"+i);
-        newTd.innerHTML = response['fcst_day_0']['hourly_data'][i+'H00']['TMP2m']
-       newTR.appendChild(newTdh);
-       newTR.appendChild(newTd);
-       document.querySelector("tbody").appendChild(newTR);
+          for ( var i=0; i<=23; i++) {
+            newTR = document.createElement("tr");
+            newTdh = document.createElement("td");
+            newTd = document.createElement("td");
+            newTdi = document.createElement("td");
+            newTdh.innerHTML =i+"H00";
+            newTd.setAttribute("id", "tmp"+i);
+            newTd.innerHTML = response['fcst_day_0']['hourly_data'][i+'H00']['TMP2m'];
+            newTdi.innerHTML = '<img src="' + response['fcst_day_0']['hourly_data'][i+'H00']['ICON'] + '"></img>';
+            newTR.appendChild(newTdh);
+            newTR.appendChild(newTd);
+            newTR.appendChild(newTdi);
+            document.querySelector("tbody").appendChild(newTR);
     
     
-    }
+          }
        }
-     }; 
+      }; 
      
     request.open("GET", "https://www.prevision-meteo.ch/services/json/paris");
     request.send();
     
      
-    }
+}
     
-    var btn = document.getElementById('ask-weather');
-    btn.addEventListener("click", askWeather);
+var btn = document.getElementById('ask-weather');
+btn.addEventListener("click", askWeather);
     
     
     
